66dce5d6e51972da963ae435f176b5ea51cbce43
Radge Carbonel <rac4843@students.uc-bcf.edu.ph>
2024-03-08 16:27:40 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TwoSum
+{
+    internal class Program
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] {i, j};
+                    }
+                }
+            }
+            return new int[] {0, 0};
+        }   
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the array: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int[] nums = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter the number: ");
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Enter the target: ");
+            int target = Convert.ToInt32(Console.ReadLine());
+            int[] result = TwoSum(nums, target);
+            if (result[0] == 0 && result[1] == 0)
+            {
+                Console.WriteLine("No result found");
+                return;
+            } else
+            {
+                Console.Write($"The result is: [{string.Join(", ", result)}]");
+            }
+        }
+    }
+}
